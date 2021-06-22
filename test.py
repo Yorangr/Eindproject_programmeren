7aca24865dadd8d348a3f8eb2c25188539f5b799
Tarik <64566489+Tarkie@users.noreply.github.com>
2021-06-22 12:42:55 +0200

    Update test.py
    class aangemaakt, en twee definities

--- test.py
+++ test.py
@@ -1,3 +1,48 @@
 #test
 
-#hallo hallo
+class Kaarten:
+    def __init__(self, aantal, symbool, kleur, vulling):
+        self.aantal = aantal
+        self.symbool = symbool
+        self.kleur = kleur
+        self.vulling = vulling
+
+    # kleur: 0 = red, 1 = green, 2 = purple
+    # symbool: 0 = diamond, 1 = squiggle, 2 = oval
+    # vulling: 0 = empty, 1 = filled, 2 = shaded
+    # aantal: 0 = 1, 1 = 2, 2 = 3
+    
+    def bestandsnaam(self):
+        gifje = '._'
+        if self.kleur == 0:
+            gifje += 'red'
+        elif self.kleur == 1:
+            gifje += 'green'
+        else:
+            gifje += 'purple'
+        if self.symbool == 0:
+            gifje += 'diamond'
+        elif self.symbool == 1:
+            gifje += 'squiggle'
+        else:
+            gifje += 'oval'
+        if self.vulling == 0:
+            gifje += 'empty'
+        elif self.vulling == 1:
+            gifje += 'filled'
+        else:
+            gifje += 'shaded'
+        if self.aantal == 0:
+            gifje += '1'
+        elif self.aantal == 1:
+            gifje += '2'
+        else:
+            gifje += '3'
+        gifje += '.gif'
+
+
+kaart1 = Kaarten(0, 2, 1, 2)
+kaart2 = Kaarten(1, 2, 2, 2)
+kaart3 = Kaarten(2, 0, 1, 1)
+
+print(kaart.vulling)
