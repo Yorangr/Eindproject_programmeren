e8b8f027a0f8da3cf88b37af7a5d55290ae83255
Yoran033 <49437644+Yoran033@users.noreply.github.com>
2021-07-02 11:27:31 +0200

    Update test.py

--- kaarten/test.py
+++ kaarten/test.py
@@ -1,8 +1,9 @@
+#import modules die nodig zijn voor de code
 import random
 import pygame
 import sys
 
-
+#Klasse kaarten om een abstracte versie van een kaart te genereren
 class Kaarten:
     def __init__(self, aantal, symbool, kleur, vulling):
         self.aantal = aantal
@@ -48,8 +49,10 @@ class Kaarten:
     
 #checken of een kaart een set is met twee andere kaarten
     def set(self,other,another):
+        #de kaarten mogen niet gelijk aan elkaar zijn
         if self==other or self == another or other == another:
             return False
+        #alle eigenschappen hetzelfde of allemaal verschillend
         elif (self.aantal==other.aantal==another.aantal or self.aantal!=other.aantal!=another.aantal!=self.aantal)\
             and (self.symbool==other.symbool==another.symbool or self.symbool!=other.symbool!=another.symbool!=self.symbool)\
             and (self.kleur==other.kleur==another.kleur or self.kleur!=other.kleur!=another.kleur!=self.kleur)\
@@ -58,6 +61,7 @@ class Kaarten:
         else:
             return False
         
+    #kaart weergeven in de terminal, is vooral handig voor testen    
     def __str__(self):
         return str((self.aantal,self.symbool,self.kleur,self.vulling))
     
@@ -76,7 +80,9 @@ def stapel():
 
 #nieuwe kaart trekken uit stapel
 def nieuwekaart(stapel,tafelkaarten):
+    #zorgen dat de while loop wordt betreden:
     nieuw=tafelkaarten[0]
+    #elke keer als nieuw al op tafel ligt, een andere kaart maken
     while nieuw in tafelkaarten:
         nieuw=random.choice(stapel)
     return nieuw
@@ -92,6 +98,7 @@ def allesets(tafelkaarten):
     for i in range(len(tafelkaarten)):
         for j in range(i+1,len(tafelkaarten)):
             for k in range(j+1,len(tafelkaarten)):
+                #alleen als de indices ongelijk zijn aan elkaar, checken dat het een set is met de set functie
                 if i!=j!=k!=i:
                     if tafelkaarten[i].set(tafelkaarten[j],tafelkaarten[k]):
                         alle_sets.append([tafelkaarten[i],tafelkaarten[j],tafelkaarten[k]])
@@ -102,27 +109,34 @@ def eenset(tafelkaarten):
     for i in range(len(tafelkaarten)):
         for j in range(i+1,len(tafelkaarten)):
             for k in range(j+1,len(tafelkaarten)):
+                #alleen als de indices ongelijk zijn aan elkaar, checken dat het een set is met de set functie
                 if i!=j!=k!=i:
                     if tafelkaarten[i].set(tafelkaarten[j],tafelkaarten[k]):
+                        #maar 1 set ipv een hele lijst zoals in allesets
                         return [tafelkaarten[i],tafelkaarten[j],tafelkaarten[k]]
      
 
-
+#stapel maken
 stapel=stapel()
+
+#kaarten voor op tafel maken
 tafelkaarten=tafelkaarten(12)
 
+#pygame spel
 
-
-#pygame spel:-----------------------
-#------------------------------------
 pygame.init()
+
+#breedte en hoogte van window
 WIDTH, HEIGHT = 1250,500
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+
+#naam van window
 pygame.display.set_caption('set')
 
 
 #kleur van achtergrond
 POKERGREEN=(0,153,0)
+
 #frames per second van spel
 FPS=60
 
@@ -133,11 +147,12 @@ extrafont = pygame.font.SysFont('Comic Sans MS', 17)
 
 #rechthoek voor tekstvakje
 input_rect=pygame.Rect(820,90,140,32)
+
+#wit
 color=(255,255,255)
 
 
-
-#getallen voor onder de kaarten
+#getallen 1 tot en met 12 voor onder de kaarten
 nummers=[]
 for i in range(1,13):
     nummers.append(myfont.render(str(i), False, (255, 255, 255)))
@@ -145,42 +160,58 @@ for i in range(1,13):
 
 #functie voor het 'tekenen' van alle dingen die nodig zijn voor het spel
 def draw_window(text_surface,invoer,submit,correct,kaart1,kaart2,kaart3,punten,compunten,text,info,tijd,kopietijd,moeilijkheid,computer,maximum):
+    
+    #achtergrond kleur
     WIN.fill(POKERGREEN)
+    
+    #stukje tekst voor onder aan het scherm
     WIN.blit(extrafont.render('(c) 2021 Tarik Tekeli en Yoran Grovenstein', False, (255,255,255)),(905,475))
+    
+    #aangeven tot hoeveel punten er wordt gespeeld 
+    #alleen aangeven als de tijd en maximum score zijn ingevuld, dus als moeilijkheid == True
     if moeilijkheid:
         if maximum==1:
             WIN.blit(extrafont.render('De eerste die 1 punt heeft wint!', False, (255,255,255)),(820,400))
         else:
             WIN.blit(extrafont.render('De eerste die '+str(maximum)+' punten heeft wint!', False, (255,255,255)),(820,400))
     
-
+    #timer tekst
     WIN.blit(myfont.render('Timer: '+text, True, (255, 255, 255)), (820, 200))
     
+    #'helaas dat is geen set' verschijnt maar tot 75% van de tijd op het moment van invoeren
     if info =='Helaas, dat is geen set' and int(text)>int(kopietijd)*0.75:
         WIN.blit(myfont.render(info, False, (255, 255, 255)), (820, 250))
+        
+    #aangeven wie heeft gewonnen verschijnt niet als de tijd loopt
     elif (info =='De computer heeft gewonnen' or info == 'Je hebt gewonnen!') and int(text)>0:
         info=''
+        
+    #'goed gedaan, dat is een set' verschijnt alleen maar tot 75% van de tijd, het zal dus verdwijnen na een bepaalde tijd
     elif int(text)>=tijd*0.75:
         WIN.blit(myfont.render(info, False, (255, 255, 255)), (820, 250))
 
-    
+    #aangeven van score
     WIN.blit(myfont.render('Jouw score: '+str(punten),False,(255,255,255)),(820,300))
     WIN.blit(myfont.render('Computer score: '+str(compunten),False,(255,255,255)),(820,350))
     
+    #gewenste tijd en maximum aantal punten alleen maar als moeilijkheid == False
     if not moeilijkheid:
         WIN.blit(myfont.render('Voer hier de gewenste tijd en',False,(255,255,255)),(820,10))
         WIN.blit(myfont.render('maximum aantal punten in:',False,(255,255,255)),(820,42))
     else:
         WIN.blit(myfont.render('Voer hier een set in:',False,(255,255,255)),(820,10))
+    
+    #tekstvakje 
     pygame.draw.rect(WIN,color,input_rect,2)
     WIN.blit(text_surface,(input_rect.x+5,input_rect.y-5))
     input_rect.w=max(100,text_surface.get_width()+10)
     
+    #genereren van plaatjes voor op het scherm
     plaatjes=[]
     for i in range(len(tafelkaarten)):
         plaatjes.append(pygame.image.load(tafelkaarten[i].plaatje()))
 
-#kaarten op het scherm laten zien    
+    #kaarten op het scherm laten zien    
     WIN.blit(plaatjes[0], (10, 10))
     WIN.blit(plaatjes[1], (150,10))
     WIN.blit(plaatjes[2], (290,10))
@@ -194,7 +225,7 @@ def draw_window(text_surface,invoer,submit,correct,kaart1,kaart2,kaart3,punten,c
     WIN.blit(plaatjes[10], (570,250))
     WIN.blit(plaatjes[11], (710,250))
 
-#getallen op het scherm laten zien    
+    #getallen op het scherm laten zien    
     WIN.blit(nummers[0],(50,205))
     WIN.blit(nummers[1],(190,205))
     WIN.blit(nummers[2],(330,205))
@@ -207,12 +238,15 @@ def draw_window(text_surface,invoer,submit,correct,kaart1,kaart2,kaart3,punten,c
     WIN.blit(nummers[9],(470,450))
     WIN.blit(nummers[10],(610,450))
     WIN.blit(nummers[11],(750,450))
-
+    
+    #scherm updaten
     pygame.display.update()
     
 
 #hoofdprogramma  
 def main():
+    
+    #alle standaardwaardes van de gebruikte variabelen
     moeilijkheid=False
     user_text=''
     invoer=''
@@ -230,21 +264,27 @@ def main():
     computer=False
     maximum=10^6
  
- #belangrijk voor de timer   
+    #belangrijk voor de timer   
     counter, text = tijd, str(tijd).rjust(3)
     pygame.time.set_timer(pygame.USEREVENT, 1000)
  
-    
+    #FPS
     clock=pygame.time.Clock()
+    
+    #game loop
     run = True
     while run:
         clock.tick(FPS)
         for event in pygame.event.get():
+            
+            #variabelen gaan weer naar standaardwaardes als iemand heeft gewonnen
             if punten>=maximum or compunten>=maximum:
                 moeilijkheid=False
                 eerstekeer=False
                 tijd=0
                 counter, text = tijd, str(tijd).rjust(3)
+                
+                #info wordt aangepast
                 if punten>=maximum:
                     info='Je hebt gewonnen!'
                 else:
@@ -252,15 +292,21 @@ def main():
                 punten=0
                 compunten=0
                 maximum=10^6
+                
             if event.type==pygame.QUIT:
                 run=False
-            ##    
-            
+             
+            #als een toets wordt ingedrukt, dan...
             if event.type==pygame.KEYDOWN:
                 
+                #als backspace wordt ingedrukt dan user_text tot één na laatste teken
                 if event.key == pygame.K_BACKSPACE:
                     user_text = user_text[:-1]
+                    
+                #als enter wordt ingedrukt
                 elif event.key == pygame.K_RETURN:
+                    
+                    #als moeilijkheid ==False, dus als tijd en maximum score nog niet zijn ingevuld
                     if not moeilijkheid:
                         invoer=user_text.split(',')
                         tijd = int(invoer[0])
@@ -270,40 +316,50 @@ def main():
                     else:
                         submit=True
                         computer=False
-                        invoer=user_text.split(',') #save user_text as invoer
+                        invoer=user_text.split(',') #sla user_text op als invoer
                         
                         kopietijd=text
-                           
+                        
+                        #maak een kopie van de kaarten die zijn ingevuld, dit geeft anders problemen bij het veranderen van kaarten
                         kaart1=tafelkaarten[int(invoer[0])-1]
                         kaart2=tafelkaarten[int(invoer[1])-1]
                         kaart3=tafelkaarten[int(invoer[2])-1]
                         
+                        #als het wel of niet een set is verander info
                         if kaart1.set(kaart2,kaart3) and int(kopietijd)!=0 and not computer:
                             info = 'Goed gedaan, dat is een set!'
                         else:
                             info ='Helaas, dat is geen set'
+                            
+                        #verander 3 kaarten en verhoog punten met 1, ook gaat de timer weer opnieuw
                         if tafelkaarten[int(invoer[0])-1].set(tafelkaarten[int(invoer[1])-1],tafelkaarten[int(invoer[2])-1]):
                             for i in range(3):
                                 tafelkaarten[int(invoer[i])-1]=nieuwekaart(stapel,tafelkaarten)                        
                             punten+=1
                             counter, text = tijd, str(tijd).rjust(3)
+                    
+                    #tekstvak wordt leeg gehaald
                     user_text = ""
                 else:
                     user_text+=event.unicode
                    
-  #timer:
+            #timer
             if event.type == pygame.USEREVENT and moeilijkheid: 
                 counter -= 1
                 if counter >= 0:
                     text = str(counter).rjust(3)
                 else:                   
                     counter, text = tijd, str(tijd).rjust(3)
+                    
+                    #als er geen set is dan wordt info aangepast en veranderen we de eerste 3 kaarten
                     if allesets(tafelkaarten)==[]:
                         if eerstekeer:
                             info = 'Er waren geen sets'
                             for i in range(3):
                                 tafelkaarten[i]=nieuwekaart(stapel,tafelkaarten)
                         eerstekeer=True
+                    
+                    #als de timer afloopt wordt de info aangepast, compunten verhoogt met 1.
                     else:
                         if eerstekeer:
                             info= 'Je was te langzaam'
@@ -311,13 +367,16 @@ def main():
                             compunten+=1
                         
                             comset=eenset(tafelkaarten)
+                            
+                            #set van commputer wordt verandert naar 3 nieuwe kaarten
                             for i in range(3):                           
                                 tafelkaarten[tafelkaarten.index(comset[i])]=nieuwekaart(stapel,tafelkaarten)
                             computer=True
                         eerstekeer=True
-                        
-                
-        text_surface=myfont.render(user_text,True,(255,255,255))    
+                                
+        text_surface=myfont.render(user_text,True,(255,255,255)) 
+        
+        #draw_window aanroepen
         draw_window(text_surface,invoer,submit,correct,kaart1,kaart2,kaart3,punten,compunten,text,info,tijd,kopietijd,moeilijkheid,computer,maximum)
         
     
